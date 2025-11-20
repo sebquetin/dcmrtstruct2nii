45a23d8346c58009567b20e1cfe08abc39d1d03f
sebquetin <sebastienquetin@hotmail.fr>
2025-11-20 17:40:41 -0500

    the reader does not work if you have other dcm files than slices in your folder.

--- dcmrtstruct2nii/adapters/input/image/dcminputadapter.py
+++ dcmrtstruct2nii/adapters/input/image/dcminputadapter.py
@@ -1,12 +1,14 @@
+import glob
+import os 
 import SimpleITK as sitk
-
+import numpy as np
 
 from dcmrtstruct2nii.adapters.input.abstractinputadapter import AbstractInputAdapter
 from dcmrtstruct2nii.exceptions import InvalidFileFormatException
 
 
 class DcmInputAdapter(AbstractInputAdapter):
-    def ingest(self, input_dir, series_id=None):
+    def ingest(self, input_dir, series_id=None, sort:bool=True):
         '''
             Load DICOMs from input_dir to a single 3D image and make sure axial
             direction is on third axis.
@@ -18,9 +20,15 @@ class DcmInputAdapter(AbstractInputAdapter):
 
         if series_id is None:
             series_id = ''
-
-        dicom_file_names = dicom_reader.GetGDCMSeriesFileNames(str(input_dir), seriesID=series_id)
-
+        dicom_file_names = [f for f in glob.glob(os.path.join(input_dir, "*.dcm")) if not (
+            os.path.basename(f).lower().startswith("rd") or \
+                os.path.basename(f).lower().startswith("rp") or \
+                    os.path.basename(f).lower().startswith("rs"))]
+        if sort:
+            origins = [sitk.ReadImage(name).GetOrigin()[2] for name in dicom_file_names]
+            if not(origins == sorted(origins)):
+                dicom_file_names = [dicom_file_names[i] for i in np.argsort(origins)]
+           
         if not dicom_file_names:
             raise InvalidFileFormatException('Directory {} is not a dicom'.format(input_dir))
 
