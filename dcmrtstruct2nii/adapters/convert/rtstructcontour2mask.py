eb604b54176f17fdea90f7f3910bf0e719538a08
sebquetin <sebastienquetin@hotmail.fr>
2025-11-20 19:20:30 -0500

    multiprocessing because it was slow

--- dcmrtstruct2nii/adapters/convert/rtstructcontour2mask.py
+++ dcmrtstruct2nii/adapters/convert/rtstructcontour2mask.py
@@ -1,21 +1,37 @@
-import tqdm 
-
+import tqdm
 import numpy as np
 from skimage import draw
 import SimpleITK as sitk
+import logging
+import multiprocessing as mp
 
 from dcmrtstruct2nii.exceptions import ContourOutOfBoundsException
 
-import logging
+
+def _process_slice(args):
+    """Worker to process all contours for a single Z slice."""
+    z, contours, shape, mask_background, mask_foreground = args
+
+    slice_mask = np.full((shape[1], shape[0]), mask_background, dtype=np.uint8)
+
+    for contour, pts in contours:
+        filled_poly = draw.polygon2mask(
+            (shape[1], shape[0]),
+            np.column_stack((pts[:, 1], pts[:, 0]))
+        )
+
+        new_mask = np.logical_xor(slice_mask == mask_foreground, filled_poly)
+        slice_mask = np.where(new_mask, mask_foreground, mask_background)
+
+    return z, slice_mask
 
 
 class DcmPatientCoords2Mask():
     def _poly2mask(self, coords_x, coords_y, shape):
-        mask = draw.polygon2mask(tuple(reversed(shape)), np.column_stack((coords_y, coords_x)))
+        return draw.polygon2mask(tuple(reversed(shape)),
+                                 np.column_stack((coords_y, coords_x)))
 
-        return mask
-
-    def convert(self, rtstruct_contours, dicom_image, mask_background, mask_foreground):
+    def convert(self, rtstruct_contours, dicom_image, mask_background, mask_foreground, multiprocessing:bool=True):
         shape = dicom_image.GetSize()
 
         mask = sitk.Image(shape, sitk.sitkUInt8)
@@ -24,40 +40,60 @@ class DcmPatientCoords2Mask():
         np_mask = sitk.GetArrayFromImage(mask)
         np_mask.fill(mask_background)
 
-        for contour in tqdm.tqdm(rtstruct_contours, total=len(rtstruct_contours), desc="Converting contours to mask"):
-            if contour['type'].upper().replace('_','').strip() not in ['CLOSEDPLANAR', 'INTERPOLATEDPLANAR', 'CLOSEDPLANARXOR']:
-                if 'name' in contour:
-                    logging.info(f'Skipping contour {contour["name"]}, unsupported type: {contour["type"]}')
-                else:
-                    logging.info(f'Skipping unnamed contour, unsupported type: {contour["type"]}')
+        slice_dict = {}
+
+        for contour in tqdm.tqdm(rtstruct_contours,
+                                 total=len(rtstruct_contours),
+                                 desc="Preparing contours"):
+
+            if contour['type'].upper().replace('_', '').strip() not in [
+                'CLOSEDPLANAR', 'INTERPOLATEDPLANAR', 'CLOSEDPLANARXOR'
+            ]:
+                name = contour.get("name", "unnamed")
+                logging.info(f'Skipping contour {name}, unsupported type: {contour["type"]}')
                 continue
 
             coordinates = contour['points']
-
             pts = np.zeros([len(coordinates['x']), 3])
 
-            for index in range(0, len(coordinates['x'])):
-                # lets convert world coordinates to voxel coordinates
-                world_coords = dicom_image.TransformPhysicalPointToContinuousIndex((coordinates['x'][index], coordinates['y'][index], coordinates['z'][index]))
-                pts[index, 0] = world_coords[0]
-                pts[index, 1] = world_coords[1]
-                pts[index, 2] = world_coords[2]
+            for index in range(len(coordinates['x'])):
+                world_coords = dicom_image.TransformPhysicalPointToContinuousIndex(
+                    (coordinates['x'][index],
+                     coordinates['y'][index],
+                     coordinates['z'][index])
+                )
+                pts[index] = world_coords
 
             z = int(round(pts[0, 2]))
 
-            try:
-                filled_poly = self._poly2mask(pts[:, 0], pts[:, 1], [shape[0], shape[1]])
-                new_mask = np.logical_xor(np_mask[z, :, :], filled_poly)
-                np_mask[z, :, :] = np.where(new_mask, mask_foreground, mask_background)
-            except IndexError as e:
-                # if this is triggered the contour is out of bounds
-                print("Exception during contour to mask conversion: ", e)
+            # keep contour + precalculated pts
+            slice_dict.setdefault(z, []).append((contour, pts))
+
+        
+        args = [
+            (z, contours, shape, mask_background, mask_foreground)
+            for z, contours in slice_dict.items()
+        ]
+        if multiprocessing:
+            # each slice processed in parallel
+            with mp.Pool(mp.cpu_count()) as pool:
+                results = list(tqdm.tqdm(
+                    pool.imap_unordered(_process_slice, args),
+                    total=len(args),
+                    desc="Converting contours to mask (parallel)"
+                ))
+        else:
+            results = []
+            for arg in tqdm.tqdm(args, total=len(args), desc="Converting contours to mask (sequential)"):
+                results.append(_process_slice(arg))
+                
+
+        # MERGE RESULTS
+        for z, slice_mask in results:
+            if 0 <= z < np_mask.shape[0]:
+                np_mask[z] = slice_mask
+            else:
                 raise ContourOutOfBoundsException()
-            except RuntimeError as e:
-                # this error is sometimes thrown by SimpleITK if the index goes out of bounds
-                if 'index out of bounds' in str(e):
-                    raise ContourOutOfBoundsException()
-                raise e  # something serious is going on
 
         mask = sitk.GetImageFromArray(np_mask)  # Avoid redundant calls by moving this here
         return mask
